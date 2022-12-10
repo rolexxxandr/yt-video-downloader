8d90d1dcbd6d048dff398314d32274b39899a731
rolexxxandr <olexxxandr.romaniuk@gmail.com>
2022-12-10 18:45:18 +0200

    added UI

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from pytube import YouTube
+from tkinter import *
 
-def Download(link):
-    youtube = YouTube(link)
-    video = youtube.streams.get_highest_resolution()
-    try:
-        video.download()
-    except:
-        print("An error has occurred")
-    print("Download is completed successfully")
+def download_highest():
+    url = YouTube(str(link.get()))
+    video = url.streams.get_highest_resolution()
+    video.download()
+    Label(window, text = 'Video downloaded successfully', font = ('Arial', 10)).grid(column=0, row=2)
 
+window = Tk()
+window.title("Downloader")
+window.geometry("300x100")
+window.resizable(False, False)
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
+link = StringVar()
+
+txt = Entry(window, width=50, textvariable=link)
+txt.grid(column=0, row=0)
+txt.focus()
+
+btn = Button(window, text="Download", bg="green", command=download_highest)
+btn.grid(column=0, row=1)
+
+window.mainloop()
